1335a295b1876d7d52bebee10e5f466b9e60ba96
Mickey Burks <brks.mck@gmail.com>
2019-07-27 15:06:31 -0700

    Add build for lambdas

---
+++ webpack.server.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    datum: './src/functions/datum.js',
+  },
+  output: {
+    path: path.resolve('./functions'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.js$/,
+            include: path.resolve('./src/functions'),
+            loader: require.resolve('babel-loader'),
+            options: {
+              cacheDirectory: true,
+              cacheCompression: true,
+              compact: true,
+            },
+          },
+          {
+            test: /\.(js|mjs)$/,
+            exclude: /@babel(?:\/|\\{1,2})runtime/,
+            loader: require.resolve('babel-loader'),
+            options: {
+              compact: true,
+              cacheDirectory: true,
+              cacheCompression: true,
+              sourceMaps: false,
+            },
+          },
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+  optimization: {
+    minimize: false,
+  },
+};
