e66803aa519d34938a12d41736750e90ae0bc4a1
Mickey Burks <brks.mck@gmail.com>
2019-07-29 22:09:22 -0700

    Debug

--- src/functions/datum.js
+++ src/functions/datum.js
@@ -3,4 +3,12 @@ import serverless from 'serverless-http';
 
 const app = aquameta();
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
+
+export const handler = function(event, context, callback) {
+  console.log({event, context});
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+  });
+} 
