7c8c99f60ab489e48e18c2af86ca51c06d5257b6
Mickey Burks <brks.mck@gmail.com>
2019-07-29 21:43:38 -0700

    Ignore encoding

--- webpack.server.js
+++ webpack.server.js
@@ -64,6 +64,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
+    new webpack.IgnorePlugin(/^encoding$/, /node-fetch/),
   ],
   optimization: {
     minimize: false,
